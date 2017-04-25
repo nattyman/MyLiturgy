7686d108efc16a5490d62bddb83256ebd84585ea
Heath Howard <heath@Heaths-MacBook-Pro.local>
2017-04-24 21:25:46 -0500

    json parsing of lectionary and matching the date POC works in utility dir

---
+++ utility/convertJson.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var users = JSON.parse(fs.readFileSync('./YearA1617.json', 'utf8'));
+
+//console.log(users[0]);
+var today = new Date();
+var year = today.getFullYear();
+var month = ("0" + (today.getMonth() + 1)).slice(-2);
+var day = today.getDate();
+var date = year+''+month+''+day;
+
+for (i=0; i<users.length; i++) {
+  console.log(users[i].DTSTART);
+  var lectDate = users[i].DTSTART
+  if(lectDate <= date) {
+    message =  users[i].SUMMARY;
+  }
+}
+console.log("Today is: " + date + ": " + message);
+
+// var file = require('./lectionaryTest.json');
+//
+// var lectionaryData = JSON.parse([file]);
+// console.log(lectionaryData);
